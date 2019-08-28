68d24cd2d7bb4d163f4477839394c24bdad2acd9
Ray Riney <rayfordriney@gmail.com>
2019-08-27 18:04:21 -0700

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var Mother = getRandomNumber();
+
+function getRandomNumber()
+{
+    return Math.floor(Math.random()*20);
+}
+    
+    
+
+function checkUserGuess() 
+{
+    var guess = document.getElementById("user-guess")["0"].value;
+    
+
+} 
+    
+
+
+        function display() {}
+
+
+        function reset() {}
